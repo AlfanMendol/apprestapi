d656c7aeb81ef02703e4140126ffd197ed785195
AlfanMendol <dikalfan@gmail.com>
2020-09-20 22:47:44 +0700

    Memanggil Routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+// Parse
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+
+// Panggil Routes
+var routes = require("./routes");
+routes(app);
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
