d32aa9797268c3594fd7cb74746481b3c3809918
AlfanMendol <dikalfan@gmail.com>
2020-09-20 23:00:51 +0700

    Tampil Mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,28 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API ku berjalan", res);
 };
+
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query("SELECT * FROM mahasiswa", function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
+
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query("SELECT * FROM mahasiswa WHERE id = ?", [id], function (
+        error,
+        rows,
+        fileds
+    ) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
