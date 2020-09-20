154e5af8098b4be3f9a2cc26ae590ab6e6b088d1
AlfanMendol <dikalfan@gmail.com>
2020-09-20 23:01:15 +0700

    Tampil Mahasiswa

--- routes.js
+++ routes.js
@@ -1,7 +1,13 @@
 "use strict";
 
+const { json } = require("body-parser");
+
 module.exports = function (app) {
     var jsonku = require("./controller");
 
     app.route("/").get(jsonku.index);
+
+    app.route("/tampil").get(jsonku.tampilsemuamahasiswa);
+
+    app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
 };
